86f8b05f878939296b31537c9f8744f97699be6f
rodrigo novais <roo.novais@gmail.com>
2020-01-14 03:54:11 -0300

    basic koa dependencies and nodemon

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import Koa, { Context, Next } from 'koa'
+import Router from 'koa-router'
+
+import bodyparser from 'koa-bodyparser'
+import cors from 'koa2-cors'
+
+const app = new Koa()
+const routes = new Router()
+
+routes.get('/', async (context: Context, next: Next) => {
+    context.status = 200
+    return next()
+})
+
+app.use(cors())
+app.use(bodyparser())
+app.use(routes.routes())
+app.use(routes.allowedMethods())
+
+app.listen(3000)
