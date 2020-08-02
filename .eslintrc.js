540edb58978d9aaa07aebeb9b646201f6c5d700e
rodrigo novais <roo.novais@gmail.com>
2020-08-02 02:10:19 -0300

    update npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,48 @@
 module.exports = {
     'env': {
         'es6': true,
-        'node': true
+        'node': true,
     },
     'extends': [
         'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended'
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:security/recommended',
     ],
     'globals': {
         'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+        'SharedArrayBuffer': 'readonly',
     },
     'parser': '@typescript-eslint/parser',
     'parserOptions': {
         'ecmaVersion': 2018,
-        'sourceType': 'module'
+        'sourceType': 'module',
     },
     'plugins': [
-        '@typescript-eslint'
+        '@typescript-eslint',
+        'security',
     ],
     'rules': {
         'indent': [
             'error',
-            4
+            4,
+            {
+                'SwitchCase': 1,
+                'FunctionDeclaration': { 'parameters': 'first' },
+                'CallExpression': { 'arguments': 'first' },
+                'ObjectExpression': 'first',
+            }
         ],
         'linebreak-style': [
             'error',
-            'unix'
+            'unix',
         ],
         'quotes': [
             'error',
-            'single'
+            'single',
         ],
         'semi': [
             'error',
-            'never'
+            'never',
         ]
     }
 }
